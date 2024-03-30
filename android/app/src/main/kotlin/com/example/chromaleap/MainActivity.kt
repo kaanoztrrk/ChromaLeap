dd563cd871d4b2f9a07d7b3203e7fb89ab7c87e2
Kaan Öztürk <kaanoztrrk411@gmail.com>
2024-03-30 15:38:13 +0300

    colorleap projecty main file add

---
+++ android/app/src/main/kotlin/com/example/chromaleap/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chromaleap
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+
+import 'app.dart';
+
+void main() {
+  runApp(GameWidget(game: ChromaLeap()));
+}
