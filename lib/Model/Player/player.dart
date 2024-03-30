ba5d7faa78d8572b70135b43a97b933449414489
Kaan Öztürk <kaanoztrrk411@gmail.com>
2024-03-30 17:35:10 +0300

    bug fix

---
+++ lib/Model/Player/player.dart
@@ -0,0 +1,43 @@
+import 'package:flame/components.dart';
+import 'package:flutter/material.dart';
+
+class Player extends PositionComponent {
+  Player({
+    this.playerRadius = 15,
+  });
+
+  final _velocity = Vector2(0, 30.0);
+  final _gravity = 980.0;
+  final _jumpSpeed = 350.0;
+
+  final double playerRadius;
+
+  @override
+  void onMount() {
+    position = Vector2(200, 100);
+    size = Vector2.all(playerRadius * 2);
+    anchor = Anchor.center;
+    super.onMount();
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    position += _velocity * dt;
+    _velocity.y += _gravity * dt;
+  }
+
+  @override
+  void render(Canvas canvas) {
+    super.render(canvas);
+    canvas.drawCircle(
+      (size / 2).toOffset(),
+      playerRadius,
+      Paint()..color = Colors.yellow,
+    );
+  }
+
+  void jump() {
+    _velocity.y = -_jumpSpeed;
+  }
+}
--- lib/app.dart
+++ lib/app.dart
@@ -3,6 +3,8 @@ import 'package:flame/events.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
+import 'Model/Player/player.dart';
+
 class ChromaLeap extends FlameGame with TapCallbacks {
   late Player player;
 
@@ -22,43 +24,5 @@ class ChromaLeap extends FlameGame with TapCallbacks {
     print('onTapDown()');
     player.jump();
     super.onTapDown(event);
-  }
-}
-
-class Player extends PositionComponent {
-  final _velocity = Vector2(0, 30.0);
-  final _gravity = 500.0;
-  final _jumpSpeed = 350.0;
-
-  @override
-  void onMount() {
-    // TODO: implement onMount
-    super.onMount();
-    position = Vector2(100, 100);
-  }
-
-  @override
-  void update(double dt) {
-    // dt => Delta Time
-    // print('update $dt');
-    // 60 Fps => 60 times in a second ==> dt : 0.16667
-    // y += speed;
-    // 120 Fps => 120 times in a second ==> dt : 0.008333333
-    // y += speed * dt;
-    position += _velocity * dt;
-    _velocity.y += _gravity * dt;
-    super.update(dt);
-  }
-
-  @override
-  void render(Canvas canvas) {
-    super.render(canvas);
-    // 60 Fps => 60 times in a second
-    canvas.drawCircle(position.toOffset(), 20,
-        Paint()..color = Color.fromARGB(255, 255, 230, 0));
-  }
-
-  void jump() {
-    _velocity.y = _jumpSpeed;
   }
 }
